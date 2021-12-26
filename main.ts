2520c792dc9b6f6dff6f0ff9e6ede3c1e41d5f37
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2021-12-26 16:41:36 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,14 +6,15 @@ const client: any = new Discord.Client();
 const API_KEY: string = readFileSync("./DEEPL_API_KEY", "utf8");
 const TOKEN: string = readFileSync("./BOT_TOKEN", "utf8");
 const prefix: string = "!";
-client.on("message", async (message) => {
+
+client.on("message", async (message:any) => {
   if (!message.content.startsWith(prefix)) return
-  const [cmd, ...args] = message.content.slice(prefix.length).split(' ')
+  const [cmd, ...args] = message.content.slice(prefix.length).split(' ');
   if (cmd == 'translate') {
     onTranslate(args, message);
   }
 });
-async function onTranslate(targetMsg: string, message) {
+async function onTranslate(targetMsg: string, message:any) {
   translate({
     free_api: true,
     text: targetMsg,
@@ -21,7 +22,13 @@ async function onTranslate(targetMsg: string, message) {
     auth_key: API_KEY
   })
     .then(result => {
+      //メッセージ送れるかは未検証
+      try{
       message.channel.send(result.data.translations[0].text);
+      } catch(e:any) {
+        console.log(e);
+      }
+      
     });
 }
 
